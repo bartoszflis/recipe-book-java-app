0f453ebe09176d0c48e0e440d24efbfbbe3cdc1c
bfl <bfl@spyro-soft.com>
2023-06-29 13:25:31 +0200

    fixes

--- src/main/java/eu/yameo/recipebookapp/RecipeBookAppApplication.java
+++ src/main/java/eu/yameo/recipebookapp/RecipeBookAppApplication.java
@@ -2,7 +2,10 @@ package eu.yameo.recipebookapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @PropertySource("classpath:application.properties")
@@ -10,6 +13,18 @@ public class RecipeBookAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RecipeBookAppApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/recipes")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
     }
 
 }
